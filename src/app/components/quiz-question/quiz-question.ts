70c2e1e92ca41d13f7a9917f037eeb7bc3ac1fbf
Asuka-Fukuchi <afukuchi24@gmail.com>
2025-11-16 17:43:18 -0800

    251116: install angular material\ created structure

---
+++ src/app/components/quiz-question/quiz-question.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-question',
+  imports: [],
+  templateUrl: './quiz-question.html',
+  styleUrl: './quiz-question.css',
+})
+export class QuizQuestion {
+
+}
---
+++ src/app/components/word-card/word-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-card',
+  imports: [],
+  templateUrl: './word-card.html',
+  styleUrl: './word-card.css',
+})
+export class WordCard {
+
+}
